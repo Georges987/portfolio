b4b62e296829f6d1aeb199554f10021d161126a5
= <=>
2023-09-04 20:22:47 +0100

    adding about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="about">
-    <v-container>
-      <v-card>
-        <v-card-title>
-          Georges AYENI
-        </v-card-title>
-        <v-card-text>
-          Hello, i'm a web developer using VueJS, Laravel and now Vuetify.
-          Ready if you need me. Contact me at +229 63 74 49 99
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary">Contact me</v-btn>
-        </v-card-actions>
-      </v-card>
+          <v-img cover height="300" src="img/upload/banner.png"> </v-img>
+          <v-container>
+              <v-avatar class="mt-n16 border" color="white" size="200">
+                <v-avatar image="/img/upload/user.jpg" size="175"></v-avatar>
+              </v-avatar>
+              <div class="text-h3 mt-4">Georges AYENI</div>
+              <p>Rédacteur web | Développeur Web | Développeur Mobile</p>
+            </v-container>
+
+            <v-container>
+          
+              <div class="text-h4">A propos de moi</div>
+
+              <p class="text-grey text-justify">
+                Passionné de l'informatique, je me suis très tôt lancé dans le développmement informatique où j'ai appris à
+                concevoir et
+                conçu de nombreuses solutions informatique. Elle sont disponible ici et sur mon profile GitHub si vous souhaitez
+                les voir.
+                En quête de nouveau défis, de nouvelle solutions, je m'investis désormais dans la résolution de problèmes. Alors si vous avez un problème à résoudre, contactez-moiet faisons finalisons votre projet ensemble. 
+              </p>
+        </v-container>
+
+      <v-container>
+          <div class="text-h4 my-4">
+            Contactez-moi !
+          </div>
+          <v-form>
+            <v-text-field
+              label="Votre email"
+              color="primary"
+            >
+            </v-text-field>
+            <v-textarea
+              label="Dites moi ce dont vous avez besoin"
+            ></v-textarea>
+          </v-form>
     </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data: (()=>{
+
+  })
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
-  <HelloWorld />
+  <div>
+    <v-container>
+      <v-img></v-img>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+
   },
 };
 </script>
