c95618813a93645c61e6fc932bd867a634692f99
= <=>
2023-09-05 09:47:22 +0100

    push des dernière modifications

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,48 +1,57 @@
 <template>
   <div class="about">
-          <v-img cover height="300" src="img/upload/banner.png"> </v-img>
-          <v-container>
-              <v-avatar class="mt-n16 border" color="white" size="200">
-                <v-avatar image="/img/upload/user.jpg" size="175"></v-avatar>
-              </v-avatar>
-              <div class="text-h3 mt-4">Georges AYENI</div>
-              <p>Rédacteur web | Développeur Web | Développeur Mobile</p>
-            </v-container>
+    <v-img cover height="300" src="img/upload/banner.png"> </v-img>
+    <v-container>
+      <v-avatar class="mt-n16 border" color="white" size="200">
+        <v-avatar image="/img/upload/user.jpg" size="175"></v-avatar>
+      </v-avatar>
+      <v-row justify="end">
+        <v-col align-self="end">
+          <div class="text-h3 mt-4">Georges AYENI</div>
+          <p>Rédacteur web | Développeur Web | Développeur Mobile</p>
+        </v-col>
+      </v-row>
+    </v-container>
 
-            <v-container>
-          
-              <div class="text-h4">A propos de moi</div>
+    <v-container>
 
-              <p class="text-grey text-justify">
-                Passionné de l'informatique, je me suis très tôt lancé dans le développmement informatique où j'ai appris à
-                concevoir et
-                conçu de nombreuses solutions informatique. Elle sont disponible ici et sur mon profile GitHub si vous souhaitez
-                les voir.
-                En quête de nouveau défis, de nouvelle solutions, je m'investis désormais dans la résolution de problèmes. Alors si vous avez un problème à résoudre, contactez-moiet faisons finalisons votre projet ensemble. 
-              </p>
-        </v-container>
+      <div class="text-h4">A propos de moi</div>
 
-      <v-container>
+      <p class="text-grey text-justify">
+        Passionné de l'informatique, je me suis très tôt lancé dans le développmement informatique où j'ai appris à
+        concevoir et
+        conçu de nombreuses solutions informatique. Elle sont disponible ici et sur mon profile GitHub si vous souhaitez
+        les voir.
+        En quête de nouveau défis, de nouvelle solutions, je m'investis désormais dans la résolution de problèmes. Alors
+        si vous avez un problème à résoudre, contactez-moiet faisons finalisons votre projet ensemble.
+      </p>
+    </v-container>
+
+    <v-container>
+      <v-row>
+        <v-col>
           <div class="text-h4 my-4">
             Contactez-moi !
           </div>
           <v-form>
-            <v-text-field
-              label="Votre email"
-              color="primary"
-            >
-            </v-text-field>
-            <v-textarea
-              label="Dites moi ce dont vous avez besoin"
-            ></v-textarea>
+            <v-text-field label="Votre email" color="primary"></v-text-field>
+            <v-textarea label="Dites moi ce dont vous avez besoin" color="primary"></v-textarea>
+            <v-btn color="primary" class="text-capitalize">
+              Envoyer
+            </v-btn>
           </v-form>
+        </v-col>
+        <v-col>
+          <v-img cover height="350" src="/img/upload/cube.jpg"></v-img>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  data: (()=>{
+  data: (() => {
 
   })
 }
